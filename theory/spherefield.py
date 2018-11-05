8805ad2fb79ee807f284e4e617db1b0e6d540d5f
David Grier <david.grier@nyu.edu>
2018-11-04 21:23:59 -0500

    load correct libraries

--- theory/spherefield.py
+++ theory/spherefield.py
@@ -1,14 +1,15 @@
-from lorenzmie.theory.sphere_coefficients import sphere_coefficients
-from lorenzmie.theory.sphericalfield import sphericalfield
+from pylorenzmie.theory.sphere_coefficients import sphere_coefficients
+from pylorenzmie.theory.sphericalfield import sphericalfield
 import numpy as np
 
-def spherefield(x, y, z, a_p, n_p, n_m = complex(1.3326, 1.5e-8), 
-                lamb = 0.447, mpp = 0.135, precision = False,
-                cartesian = False):
+
+def spherefield(x, y, z, a_p, n_p, n_m=complex(1.3326, 1.5e-8),
+                lamb=0.447, mpp=0.135, precision=False,
+                cartesian=False):
     """
     Calculate the complex electric field scattered by a sphere illuminated
     by a plane wave linearly polarized in the x direction.
-   
+
     Args:
         x:  [npts] array of pixel coordinates [pixels]
         y:  [npts] array of pixel coordinates [pixels]
@@ -31,16 +32,15 @@ def spherefield(x, y, z, a_p, n_p, n_m = complex(1.3326, 1.5e-8),
     """
     ab = sphere_coefficients(a_p, n_p, n_m, lamb)
 
-    if precision: 
+    if precision:
         # retain first coefficient for bookkeeping
         fac = abs(ab[:, 1])
         w = np.where(fac > precision*max(fac))
-        w = np.concatenate((np.array([0]),w[0])) 
-        ab =  ab[w,:]
- 
-    lamb_m = lamb/np.real(n_m)/mpp # medium wavelength [pixel]
-    field = sphericalfield(x, y, z, ab, lamb_m, cartesian=cartesian, 
-                           str_factor = False)
+        w = np.concatenate((np.array([0]), w[0]))
+        ab = ab[w, :]
+
+    lamb_m = lamb/np.real(n_m)/mpp  # medium wavelength [pixel]
+    field = sphericalfield(x, y, z, ab, lamb_m, cartesian=cartesian,
+                           strength=False)
 
     return field
-
