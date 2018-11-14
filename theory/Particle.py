73edd58efe5456d7ef234498d08f6bd82acae300
David Grier <david.grier@nyu.edu>
2018-11-14 11:55:03 -0500

    support for python2 and 3

--- theory/Particle.py
+++ theory/Particle.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import json
+from future.utils import iteritems
 
 
 class Particle(object):
@@ -89,7 +90,7 @@ class Particle(object):
 
     @properties.setter
     def properties(self, properties):
-        for name, value in properties.iteritems():
+        for (name, value) in iteritems(properties):
             if hasattr(self, name):
                 setattr(self, name, value)
 
