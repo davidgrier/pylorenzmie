e0f5a97215c6c12077c7307d0fa7e1e63052dd41
David Grier <david.grier@nyu.edu>
2018-11-25 09:56:18 -0500

    interactive hologram analysis

---
+++ lmtool/QDoubleSlider.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QSlider
+from PyQt5.QtCore import (pyqtSignal, pyqtSlot)
+
+
+class QDoubleSlider(QSlider):
+
+    valueChanged = pyqtSignal(float)
+
+    def __init__(self, *args, **kwargs):
+        super(QDoubleSlider, self).__init__(*args, **kwargs)
+        self._imin = 0.
+        self._imax = 10000.
+        super(QDoubleSlider, self).setMinimum(int(self._imin))
+        super(QDoubleSlider, self).setMaximum(int(self._imax))
+        self._min = 0.
+        self._max = 100.
+        super(QDoubleSlider, self).valueChanged[int].connect(
+            self.reemitValueChanged)
+
+    def _convert_i2f(self, ivalue):
+        frac = float(ivalue - self._imin)/(self._imax - self._imin)
+        return frac * (self._max - self._min) + self._min
+
+    def _convert_f2i(self, value):
+        frac = (value - self._min)/(self._max - self._min)
+        return int(frac * (self._imax - self._imin) + self._imin)
+
+    @pyqtSlot(int)
+    def reemitValueChanged(self, ivalue):
+        value = self._convert_i2f(ivalue)
+        self.valueChanged[float].emit(value)
+
+    def value(self):
+        ivalue = float(super(QDoubleSlider, self).value())
+        return self._convert_i2f(ivalue)
+
+    @pyqtSlot(float)
+    def setValue(self, value):
+        ivalue = self._convert_f2i(value)
+        super(QDoubleSlider, self).setValue(ivalue)
+
+    def setMinimum(self, value):
+        self.setRange(value, self._max)
+
+    def setMaximum(self, value):
+        self.setRange(self._min, value)
+
+    def setRange(self, minimum, maximum):
+        ovalue = self.value()
+        self._min = minimum
+        self._max = maximum
+        self.setValue(ovalue)
---
+++ lmtool/QParameterWidget.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtWidgets
+from QDoubleSlider import QDoubleSlider
+
+
+class QParameterWidget(QtWidgets.QWidget):
+    '''Widget for adjusting a floating-point parameter
+
+    QParameterWidget combines a QDoubleSpinBox with
+    a custom QDoubleSlider to set and adjust the value
+    of a parameter.  The name of the parameter is displayed
+    with a QLabel.  The parameter can be fixed with a
+    QCheckBox, which also disables the controls.
+
+    ...
+
+    Attributes
+    ----------
+    spinbox : QDoubleSpinBox
+        Widget that displays and controls the parameter
+    slider : QDoubleSlider
+        Widget for continuously adjusting the parameter
+    checkbox : QCheckBox
+        Checkbox for fixing the parameter
+    fixed : bool
+        True if parameter is fixed
+
+    Methods
+    -------
+    minimum() : float
+        Minimum value for parameter
+    maximum() : float
+        Maximum value for parameter
+    value() : float
+        Current value of parameter
+    decimals() : int
+        Number of decimal places to display
+
+    Slots
+    -----
+    setValue(float) :
+        Sets the parameter value
+
+    Signals
+    -------
+    valueChanged(float) :
+        Emitted when parameter value is changed
+    '''
+
+    def __init__(self,
+                 parent=None,
+                 text='parameter',
+                 minimum=0,
+                 maximum=100,
+                 value=50,
+                 decimals=3, **kwargs):
+        super(QParameterWidget, self).__init__(parent, **kwargs)
+        self.setupUI()
+        self.setupAPI()
+        self.setText(text)
+        self.setMinimum(minimum)
+        self.setMaximum(maximum)
+        self.setValue(value)
+        self.setDecimals(decimals)
+
+    def setupUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.spinbox = QtWidgets.QDoubleSpinBox(self)
+        self.checkbox = QtWidgets.QCheckBox(self)
+        self.slider = QDoubleSlider(self)
+        self.slider.setOrientation(QtCore.Qt.Horizontal)
+
+        self.layout = QtWidgets.QGridLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setHorizontalSpacing(4)
+        self.layout.setVerticalSpacing(0)
+        self.layout.addWidget(self.label, 0, 0, 1, 1)
+        self.layout.addWidget(self.spinbox, 0, 1, 1, 1)
+        self.layout.addWidget(self.checkbox, 0, 2, 1, 1)
+        self.layout.addWidget(self.slider, 1, 0, 1, 3)
+
+        self.spinbox.valueChanged['double'].connect(self.slider.setValue)
+        self.slider.valueChanged['double'].connect(self.spinbox.setValue)
+        self.checkbox.stateChanged.connect(self.fixValue)
+
+    def setupAPI(self):
+        # Methods
+        self.text = self.label.text
+        self.setText = self.label.setText
+        self.minimum = self.spinbox.minimum
+        self.maximum = self.spinbox.maximum
+        self.decimals = self.spinbox.decimals
+        self.setDecimals = self.spinbox.setDecimals
+        self.value = self.spinbox.value
+
+        # Slots
+        self.setValue = self.spinbox.setValue
+
+        # Signals
+        self.valueChanged = self.spinbox.valueChanged
+
+    @QtCore.pyqtSlot(int)
+    def fixValue(self, state):
+        self.spinbox.setDisabled(state)
+        self.slider.setDisabled(state)
+
+    @property
+    def fixed(self):
+        '''Parameter cannot be changed if True'''
+        return self.checkbox.isChecked()
+
+    @fixed.setter
+    def fixed(self, state):
+        self.checkbox.setChecked(state)
+
+    def setMinimum(self, min):
+        self.spinbox.setMinimum(min)
+        self.slider.setMinimum(min)
+
+    def setMaximum(self, max):
+        self.spinbox.setMaximum(max)
+        self.slider.setMaximum(max)
+
+    def setRange(self, min, max):
+        self.spinbox.setRange(min, max)
+        self.slider.setRange(min, max)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    param = QParameterWidget(text='a<sub>p</sub>')
+    param.setRange(0.3, 10)
+    param.setValue(0.75)
+    param.setDecimals(3)
+    param.spinbox.setSuffix(' μm')
+    param.show()
+    sys.exit(app.exec_())
