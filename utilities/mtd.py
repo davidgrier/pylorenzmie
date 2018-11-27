35702acaa5053c2c38b4e42409275e3f04da7653
laltman2 <laurenealtman@gmail.com>
2018-11-27 17:30:47 -0500

    changed formatting for YOLO

--- utilities/mtd.py
+++ utilities/mtd.py
@@ -3,7 +3,8 @@
 
 '''Make Training Data'''
 import sys
-
+sys.path.append('/home/lea336/pylorenzmie/')
+sys.path.append('/home/lea336/')
 import json
 try:
     from pylorenzmie.theory.CudaLMHologram import CudaLMHologram as LMHologram
@@ -30,11 +31,7 @@ def feature_extent(sphere, config, nfringes=20, maxrange=300):
     # roughly estimate radii of zero crossings
     b = h.hologram() - 1.
     ndx = np.where(np.diff(np.sign(b)))[0] + 1
-<<<<<<< HEAD
-    if len(ndx) < (nfringes+1):
-=======
     if len(ndx) <= nfringes:
->>>>>>> 9806440363da6f76baf353ea37ecf1006291d0b9
         return maxrange
     else:
         return float(ndx[nfringes])
@@ -103,14 +100,16 @@ def mtd(configfile='mtd.json'):
     # create directories and filenames
     directory = os.path.expanduser(config['directory'])
     imgtype = config['imgtype']
-    for dir in ('images', 'params', 'labels'):
+    for dir in ('images_labels', 'params'):
         if not os.path.exists(os.path.join(directory, dir)):
             os.makedirs(os.path.join(directory, dir))
     shutil.copy2(configfile, directory)
-    imgname = os.path.join(directory, 'images', 'image{:04d}.' + imgtype)
+    filetxtname = os.path.join(directory, 'filenames.txt')
+    imgname = os.path.join(directory, 'images_labels', 'image{:04d}.' + imgtype)
     jsonname = os.path.join(directory, 'params', 'image{:04d}.json')
-    yoloname = os.path.join(directory, 'labels', 'image{:04d}.txt')
+    yoloname = os.path.join(directory, 'images_labels' , 'image{:04d}.txt')
 
+    filetxt = open(filetxtname, 'w')
     for n in range(config['nframes']):  # for each frame ...
         print(imgname.format(n))
         sample = make_sample(config)   # ... get params for particles
@@ -128,6 +127,7 @@ def mtd(configfile='mtd.json'):
             fp.write(format_json(sample, config))
         with open(yoloname.format(n), 'w') as fp:
             fp.write(format_yolo(sample, config))
+        filetxt.write(imgname.format(n) + '\n')
 
 
 if __name__ == '__main__':
