61e7e85d9ddc3f659704be5760ccc54f46a009dc
mjo22 <michaelobrien@nyu.edu>
2019-07-12 13:09:13 -0400

    add penalty  to nelder-mead when algorithm goes outside bounds

--- fitting/minimizers.py
+++ fitting/minimizers.py
@@ -32,7 +32,7 @@ def amoebas(objective, params, initial_simplex=None, maxevals=int(1e3),
             simplex = np.vstack([x0, np.diag(scale) + x0])
             # Make initial guess centroid of simplex
             xbar = np.add.reduce(simplex[:-1], 0) / N
-            simplex = simplex - (xbar - x0)
+            # simplex = simplex - (xbar - x0)
             initial_simplex.append(simplex)
     minresult = None
     minchi = np.inf
@@ -50,8 +50,8 @@ def amoebas(objective, params, initial_simplex=None, maxevals=int(1e3),
         if result.redchi < minchi:
             minresult = result
             minchi = result.redchi
-        #report_fit(result)
-    
+        # report_fit(result)
+
     '''
     chis = []
     for idx, simplex in enumerate(initial_simplex):
@@ -67,8 +67,8 @@ def amoebas(objective, params, initial_simplex=None, maxevals=int(1e3),
     return minresult
 
 
-def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
-           simplex_scale=.1, xtol=1e-7, ftol=1e-7, adaptive=True):
+def amoeba(objective, params, bounds, maxevals=int(1e3), initial_simplex=None,
+           simplex_scale=.1, xtol=1e-7, ftol=1e-7, adaptive=False):
     '''Nelder-mead optimization adapted from scipy.optimize.fmin'''
     parameters = list(params.keys())
     if type(simplex_scale) == list:
@@ -76,7 +76,8 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
     # Raise exception if any params are unbounded
     for param in parameters:
         if params[param].vary:
-            min, max = (params[param].min, params[param].max)
+            #min, max = (params[param].min, params[param].max)
+            min, max = (bounds[param][0], bounds[param][1])
             if not np.isfinite(min) or not np.isfinite(max):
                 msg = "Nelder-mead requires finite parameter bounds"
                 raise ValueError(msg)
@@ -87,6 +88,12 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
             if params[param].vary:
                 temp.append(xtol[param])
     xtol = np.array(temp)
+    temp = []
+    if type(simplex_scale) == dict:
+        for param in parameters:
+            if params[param].vary:
+                temp.append(simplex_scale[param])
+    simplex_scale = np.array(temp)
     # Initialize first guess normalization
     x0, scale, offset = ([], [], [])
     init_vals = {}
@@ -98,37 +105,42 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
                          params[param].min)
             offset.append(params[param].min)
     x0, scale, offset = (np.array(x0), np.array(scale), np.array(offset))
+    scale = None
     # Normalize
-    if type(simplex_scale) is np.ndarray:
-        simplex_scale /= scale
-    if type(xtol) is np.ndarray:
-        xtol /= scale
-    x0 = (x0 - offset) / scale
+    # if type(simplex_scale) is np.ndarray:
+    #    simplex_scale /= scale
+    # if type(xtol) is np.ndarray:
+    #    xtol /= scale
+    # x0 = (x0 - offset) / scale
     # Initialize simplex
     N = len(x0)
     if initial_simplex is None:
         if type(simplex_scale) is float:
             simplex_scale = np.full(N, simplex_scale)
+        print(simplex_scale)
         simplex = np.vstack([x0, np.diag(simplex_scale) + x0])
         # Make initial guess centroid of simplex
         xbar = np.add.reduce(simplex[:-1], 0) / N
-        simplex = simplex - (xbar - x0)
+        # simplex = simplex - (xbar - x0)
     else:
         if initial_simplex.shape != (N+1, N):
             raise ValueError("Initial simplex must be dimension (N+1, N)")
-        simplex = (initial_simplex - offset) / scale
+        # simplex = (initial_simplex - offset) / scale
+        simplex = initial_simplex
     # Initialize algorithm
     N = len(x0)
-    clip = np.clip
     maxevals = maxevals
+    penalty = 1000
     neval = 1
     niter = 1
     one2np1 = list(range(1, N + 1))
     evals = np.zeros(N+1, float)
     for idx in range(N+1):
-        params = _updateParams(simplex[idx], params,
-                               scale, offset)
+        params, penalize = _updateParams(simplex[idx], params,
+                                         bounds, scale, offset)
         evals[idx] = objective(params)
+        if penalize:
+            evals[idx] *= penalty
         neval += 1
     idxs = np.argsort(evals)
     evals = np.take(evals, idxs, 0)
@@ -145,10 +157,6 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
         chi = 2
         psi = 0.5
         sigma = 0.5
-    rho = 1
-    chi = 2.
-    psi = 0.5
-    sigma = 0.5
 
     # START FITTING
     while(neval < maxevals):
@@ -157,20 +165,23 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
             break
         # Reflect
         xbar = np.add.reduce(simplex[:-1], 0) / N
-        xr = clip((1 + rho) * xbar - rho * simplex[-1], 0.01, 1)
-        params = _updateParams(xr, params,
-                               scale, offset)
+        xr = (1 + rho) * xbar - rho * simplex[-1]
+        params, penalize = _updateParams(xr, params, bounds,
+                                         scale, offset)
         fxr = objective(params)
+        if penalize:
+            fxr *= penalty
         neval += 1
         doshrink = 0
         # Check if reflection is better than best estimate
         if fxr < evals[0]:
             # If so, reflect double and see if that's even better
-            xe = clip((1 + rho * chi) * xbar - rho *
-                      chi * simplex[-1], 0.01, 1)
-            params = _updateParams(xe, params,
-                                   scale, offset)
+            xe = (1 + rho * chi) * xbar - rho * chi * simplex[-1]
+            params, penalize = _updateParams(xe, params, bounds,
+                                             scale, offset)
             fxe = objective(params)
+            if penalize:
+                fxe *= penalty
             neval += 1
             if fxe < fxr:
                 simplex[-1] = xe
@@ -185,11 +196,12 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
             else:
                 # If reflection is not better, contract.
                 if fxr < evals[-1]:
-                    xc = clip((1 + psi * rho) * xbar - psi *
-                              rho * simplex[-1], 0.01, 1)
-                    params = _updateParams(xc, params,
-                                           scale, offset)
+                    xc = (1 + psi * rho) * xbar - psi * rho * simplex[-1]
+                    params, penalize = _updateParams(xc, params, bounds,
+                                                     scale, offset)
                     fxc = objective(params)
+                    if penalize:
+                        fxc *= penalty
                     neval += 1
                     if fxc <= fxr:
                         simplex[-1] = xc
@@ -198,10 +210,12 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
                         doshrink = 1
                 else:
                     # Do 'inside' contraction
-                    xcc = clip((1 - psi) * xbar + psi * simplex[-1], 0.01, 1)
-                    params = _updateParams(xcc, params,
-                                           scale, offset)
+                    xcc = (1 - psi) * xbar + psi * simplex[-1]
+                    params, penalize = _updateParams(xcc, params, bounds,
+                                                     scale, offset)
                     fxcc = objective(params)
+                    if penalize:
+                        fxcc *= penalty
                     neval += 1
                     if fxcc < evals[-1]:
                         simplex[-1] = xcc
@@ -212,18 +226,19 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
                     for j in one2np1:
                         simplex[j] = simplex[0] + sigma * \
                             (simplex[j] - simplex[0])
-                        simplex[j] = clip(simplex[j], 0.01, 1)
-                        params = _updateParams(simplex[j], params,
-                                               scale, offset)
+                        params, penalize = _updateParams(simplex[j], params, bounds,
+                                                         scale, offset)
                         evals[j] = objective(params)
+                        if penalize:
+                            evals[j] *= penalty
                         neval += 1
         idxs = np.argsort(evals)
         simplex = np.take(simplex, idxs, 0)
         evals = np.take(evals, idxs, 0)
 
         niter += 1
-    params = _updateParams(simplex[0], params,
-                           scale, offset)
+    params, penalize = _updateParams(simplex[0], params, bounds,
+                                     scale, offset)
     chi = evals[0]
     success = False if neval == maxevals else True
     result = MinimizerResult(params=params,
@@ -236,13 +251,20 @@ def amoeba(objective, params, maxevals=int(1e3), initial_simplex=None,
     return result
 
 
-def _updateParams(xn, params, scale, offset):
-    x = xn * scale + offset
+def _updateParams(x, params, bounds, scale, offset):
+    # print(x)
+    # x = xn * scale + offset
     parameters = list(params.keys())
     varying = []
     for param in parameters:
         if params[param].vary:
             varying.append(param)
+    penalize = False
     for idx in range(len(x)):
         params[varying[idx]].value = x[idx]
-    return params
+        if bounds[varying[idx]][1] < params[varying[idx]].value:
+            penalize = True
+        if bounds[varying[idx]][0] > params[varying[idx]].value:
+            penalize = True
+    print(penalize)
+    return params, penalize
