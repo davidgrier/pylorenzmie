863006fa7035fef5fa55374ea06299c9b9af437f
mjo22 <michaelobrien@nyu.edu>
2019-07-17 16:18:43 -0400

    initial commit, FitResult

---
+++ fitting/FitResult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class FitResult(object):
+
+    def __init__(self, scipyresult):
+        self.scipyresult = scipyresult
+
+    def getReport(self):
+        pass
+
+    def save(self):
+        pass
