d7c3220e012b68dca7271202af3f0dc61fba0f8a
mjo22 <michaelobrien@nyu.edu>
2019-08-09 02:02:07 -0400

    Begin attempt at cython speedup

--- fitting/__init__.py
+++ fitting/__init__.py
@@ -1,1 +1,12 @@
-__all__ = ['mie_fit.py', 'minimizers.py']
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+try:
+    from pylorenzmie.fitting.cython.cminimizers import amoeba
+except Exception as e:
+    print(e)
+    from pylorenzmie.fitting.minimizers import amoeba
+
+all = ['mie_fit.py', 'minimizers.py', amoeba]
