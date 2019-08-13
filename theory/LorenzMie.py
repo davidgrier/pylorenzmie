b2d90aae57cab981d651331340b48c60f8479c54
laltman2 <laurenealtman@gmail.com>
2019-08-13 17:50:14 -0400

    multi-particle field computation now working on cuda

--- theory/LorenzMie.py
+++ theory/LorenzMie.py
@@ -49,10 +49,43 @@ class LorenzMie(GeneralizedLorenzMie):
             coordinates of sphere center: (x_p, y_p, z_p)
         '''
         super(LorenzMie, self).__init__(**kwargs)
-        self.particle = Sphere()
-        if a_p is not None:
-            self.particle.a_p = a_p
-        if n_p is not None:
-            self.particle.n_p = n_p
-        if r_p is not None:
-            self.particle.r_p = r_p
+        if particle is None:
+            self.particle = Sphere()
+            if a_p is not None:
+                self.particle.a_p = a_p
+            if n_p is not None:
+                self.particle.n_p = n_p
+            if r_p is not None:
+                self.particle.r_p = r_p
+        else:
+            self.particle = particle
+
+if __name__=='__main__':
+    import numpy as np
+    import cupy as cp
+    from matplotlib import pyplot as plt
+    from pylorenzmie.theory.Instrument import Instrument, coordinates
+    
+    particle = Sphere()
+    particle.r_p = [150, 150, 200]
+    particle.a_p = 0.5
+    particle.n_p = 1.45
+    particle2 = Sphere()
+    particles = [particle, particle2]
+    # Form image with default instrument                                                                                             
+    instrument = Instrument()
+    instrument.magnification = 0.135
+    instrument.wavelength = 0.447
+    instrument.n_m = 1.335
+    # Use Generalized Lorenz-Mie theory to compute field                                                                             
+    kernel = LorenzMie(particle = particles)
+    kernel.instrument = instrument
+    kernel.coordinates = coordinates((201, 201))
+    field = kernel.field()
+    # Compute hologram from field and show it                                                                                        
+    field[0, :] += 1.
+    hologram = cp.sum(cp.real(field * cp.conj(field)), axis=0)
+    hologram = hologram.get()
+    plt.imshow(hologram.reshape(201, 201), cmap='gray')
+    plt.show()
+
