e1a80c324cd1d7daabf59b9b94301270983263af
mjo22 <michaelobrien@nyu.edu>
2019-08-21 11:53:34 -0400

    Functioning cythonized nelder-mead

--- fitting/cython/cminimizers.pyx
+++ fitting/cython/cminimizers.pyx
@@ -1,24 +1,21 @@
 from scipy.optimize import OptimizeResult
 import numpy as np
-# cimport numpy as np
-cimport cython
+import cython
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def amoeba(objective, x0, xmin, xmax,
-           simplex_scale, xtol, maxevals=int(1e3),
-           initial_simplex=None, ftol=1e-7, adaptive=False):
+def amoeba(objective, x0, xmin, xmax, maxevals=int(1e3), initial_simplex=None,
+           simplex_scale=.1, xtol=1e-7, ftol=1e-7, adaptive=False):
     '''Nelder-mead optimization adapted from scipy.optimize.fmin'''
-    simplex_scale = np.asarray(simplex_scale,
-                               dtype=np.float64)
-    xtol = np.asarray(xtol, dtype=np.float64)
+    simplex_scale = np.asarray(simplex_scale)
+    xtol = np.asarray(xtol)
     # Initialize simplex
     N = len(x0)
     if initial_simplex is None:
-        simplex = np.zeros((N+1, N))
-        simplex[0] = x0
-        simplex[1:] = np.diag(simplex_scale) + x0
+        if simplex_scale.size == 1:
+            simplex_scale = np.full(N, simplex_scale[0])
+        simplex = np.vstack([x0, np.diag(simplex_scale) + x0])
     else:
         if initial_simplex.shape != (N+1, N):
             raise ValueError("Initial simplex must be dimension (N+1, N)")
@@ -28,7 +25,7 @@ def amoeba(objective, x0, xmin, xmax,
     neval = 1
     niter = 1
     one2np1 = list(range(1, N + 1))
-    evals = np.zeros(N+1)
+    evals = np.zeros(N+1, float)
     for idx in range(N+1):
         simplex[idx] = np.maximum(xmin, np.minimum(simplex[idx], xmax))
         evals[idx] = objective(simplex[idx])
@@ -69,8 +66,8 @@ def amoeba(objective, x0, xmin, xmax,
             message = 'failure (stuck to boundary)'
             break
         # Reflect
-        xbar = np.add.reduce(simplex[:N-1], 0) / N
-        xr = (1 + rho) * xbar - rho * simplex[N-1]
+        xbar = np.add.reduce(simplex[:N], 0) / N
+        xr = (1 + rho) * xbar - rho * simplex[N]
         xr = np.maximum(xmin, np.minimum(xr, xmax))
         fxr = objective(xr)
         neval += 1
@@ -78,41 +75,41 @@ def amoeba(objective, x0, xmin, xmax,
         # Check if reflection is better than best estimate
         if fxr < evals[0]:
             # If so, reflect double and see if that's even better
-            xe = (1 + rho * chi) * xbar - rho * chi * simplex[N-1]
+            xe = (1 + rho * chi) * xbar - rho * chi * simplex[N]
             xe = np.maximum(xmin, np.minimum(xe, xmax))
             fxe = objective(xe)
             neval += 1
             if fxe < fxr:
-                simplex[N-1] = xe
-                evals[N-1] = fxe
+                simplex[N] = xe
+                evals[N] = fxe
             else:
-                simplex[N-1] = xr
-                evals[N-1] = fxr
+                simplex[N] = xr
+                evals[N] = fxr
         else:
-            if fxr < evals[N-2]:
-                simplex[N-1] = xr
-                evals[N-1] = fxr
+            if fxr < evals[N-1]:
+                simplex[N] = xr
+                evals[N] = fxr
             else:
                 # If reflection is not better, contract.
-                if fxr < evals[N-1]:
-                    xc = (1 + psi * rho) * xbar - psi * rho * simplex[N-1]
+                if fxr < evals[N]:
+                    xc = (1 + psi * rho) * xbar - psi * rho * simplex[N]
                     xc = np.maximum(xmin, np.minimum(xc, xmax))
                     fxc = objective(xc)
                     neval += 1
                     if fxc <= fxr:
-                        simplex[N-1] = xc
-                        evals[N-1] = fxc
+                        simplex[N] = xc
+                        evals[N] = fxc
                     else:
                         doshrink = 1
                 else:
                     # Do 'inside' contraction
-                    xcc = (1 - psi) * xbar + psi * simplex[N-1]
+                    xcc = (1 - psi) * xbar + psi * simplex[N]
                     xcc = np.maximum(xmin, np.minimum(xcc, xmax))
                     fxcc = objective(xcc)
                     neval += 1
-                    if fxcc < evals[N-1]:
-                        simplex[N-1] = xcc
-                        evals[N-1] = fxcc
+                    if fxcc < evals[N]:
+                        simplex[N] = xcc
+                        evals[N] = fxcc
                     else:
                         doshrink = 1
                 if doshrink:
