84dc4e0a29a224b463359340912e4fac60d0064f
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-16 18:07:49 -0400

    Re-merge add features and add bboxes, general clean up
    - Added updated documentation
    - recombined add features and add bboxes into one add
    - updated getters and setters for image and image path 
    -added crop() method: use Frame's bboxes to crop image and set corresponding features' data
    
    To-Do: Document + clean up Serialize and Deserialize

--- analysis/Frame.py
+++ analysis/Frame.py
@@ -6,8 +6,41 @@ import numpy as np
 from .Feature import Feature
 
 class Frame(object):
+    '''
+    Abstraction of an experimental video frame
+    ...
+    Attributes
+    ----------
+    image : numpy.ndarray
+        Image from camera. Setter ensures the image is grayscale. Directly choosing an image sets image_path to None. 
+    framenumber : int
+    image_path : string    
+        The setter will attempt to read the image from file at image_path. Additionally, if the Frame 
+        does not have a framenumber, the setter will attempt to read one from the end of the string.
+    bboxes : List of tuples ( {x, y, w, h} )
+        Bounding box of dimensions (w, h) around feature at (x, y). Used for cropping to obtain image stamps
+    features : List of Feature objects
+        
+    Methods
+    ------- 
+    add(features, info=None) : None
+        features: (list of) Feature objects / list of serialized Features (dicts) / or list of bboxes (tuples)
+        optional: info: (list of) serialized feature info (dicts)
+            - Unpack and add (each) Feature to the Frame, 
+            - if (each) input is a bbox, add it to Frame; otherwise, add None to frame's bbox list
+            - deserialize info into Feature(s), if info passed
+            
+    crop(all) : None
+        - Use each (not-None) bbox to crop the frame's image and update the corresponding feature's data
+        all : boolean
+            if all is False (default), then only update Features which do not already have data. 
+        
+    
+  
+    '''
 """
-    Object representation of an experimental video frame. Frames can have an image (data), a framenumber, an instrument for fitting, a list of Feature objects
+    Object representation of an experimental video frame. 
+    Attributes:Frames can have an image, a framenumber, an instrument for fitting, a list of Feature objects
     and a corresponding list of bounding boxes. Features can be added in two ways: 
       1) If the frame has an image, then Features are added by specifying a bbox (via a dict) in deserialize . The bbox specifies x_p and y_p, and 
          Feature data (cropped image) is obtained by using the bbox to crop the Frame data. Optionally, the dict can also pass other feature info 
@@ -47,43 +80,43 @@ class Frame(object):
     def image_path(self):
         return self._image_path
     
-    @image_path.setter                   #### Method for reading an image path into a frame
+    @image_path.setter                   
     def image_path(self, path):
-        image = cv2.imread(path, 0)      #### 0 is grayscale
-        if image is not None:            #### If this path is valid, then read framenumber for the path (unless Frame already has a framenumber) 
-            self.framenumber = int(path[:-8:-4]) if self.framenumber is not None else self.framenumber
-        elif self.framenumber is not None:
-            image = cv2.imread(path + 'image'+ str(self.framenumber).rjust(4, '0')+'.png') 
-        if image is not None
-            self.image = image
-            self._image_path = path
+        if not isinstance(path, String):
+            self.image = None                                                   #### If invalid format, clear image and path
+            if path is not None:
+                print('Warning: could not read path of type {}'.print(type(path)))
+            return
+        if path[-1] is '/' and self.framenumber is not None:                    #### If path is a directory, look for image of format 'path/image(framenumber).png'
+            path = path + 'image'+ str(self.framenumber).rjust(4, '0')+'.png'    
+        self.image = cv2.imread(path, 0)                                        #### Read image (0 is grayscale)
+        if self.image is not None:
+            self._image_path = path                                             #### If an image was found, then keep the path
+            if self.framenumber is None:                                        #### If path leads to an image and framenumber isn't set, try to
+                self.framenumber = int(path[:-8:-4])                            ####    read framenumber from the path  (i.e. 0107 from '...image0107.png')                 
         else:
-            self._image_path = None
-            print("Warning: Could not read image from path '{}'".format(im))
-            
+            print("Warning: image not found at path '{}'".format(path))
+
     @property
     def image(self):
         return self._image
     
     @image.setter(self):
     def image(self, image):
-        if isinstance(image, String)
-            self.image_path = image    
-        elif isinstance(image, np.ndarray):
-            self._image_path = None
-            if len(np.shape(image)) is 2:
-                self._image = image
-            elif len(np.shape(image)) is 3:
-                self._image = image[:, :, 0]
-            else:
-                self._image = None
-                print("Warning: invalid image dimensions: {}".format(np.shape(image)))            
+        self._image_path = None
+        if not isinstance(image, np.ndarray):
+            self._image = None
+            if image is not None:
+                print("Warning: could not read image of type {}".format(type(im)))
+        elif len(np.shape(image)) is 2:
+            self._image = image
+        elif len(np.shape(image)) is 3:
+            self._image = image[:, :, 0]
         else:
             self._image = None
-            self._image_path = None
-            if image is not None:
-                print("Warning: invalid image format: {}".format(type(im)))
-                
+            print("Warning: invalid image dimensions: {}".format(np.shape(image)))            
+         self.crop(all=True) 
+    
     @property
     def features(self):
         return self._features
@@ -92,41 +125,52 @@ class Frame(object):
     def bboxes(self):
         return self._bboxes
     
-    def add(self, features): 
+    def add(self, features, info=None): 
         if features is None:
             return
-        if type(features) is Feature:
+        if not isinstance(features, list):                         #### Ensure input is a list
             features = [features] 
-        for feature in features:
-            if self.instrument is not None:
-                feature.model.instrument = self.instrument
-            if type(feature) is Feature:
+        if not isinstance(info, list):
+            info = [info for feature in features]
+        for i, feature in enumerate(features):
+            if isinstance(feature, tuple) and len(feature) is 4:    #### case where bbox passed
+                bbox = feature
+                feature = Feature()  
+            elif isinstance(feature, dict):                         #### case where serialized feature passed
+                bbox = None
+                feature = Feature().deserialize(info=feature)       #### case where Feature object passed
+            elif isinstance(feature, Feature):
+                bbox = None
+            if isinstance(feature, Feature):                        #### If a feature was found, add it
+                self._bboxes.append(bbox)
+                if self.instrument is not None:
+                    feature.model.instrument = self.instrument
+                if info[i] is not None:
+                    feature.deserialize(info=info[i])
                 self._features.append(feature)
-                self._bboxes.append(None)
-            else:
-                msg = "features must be list of Features"
-                msg += " or deserializable Features"
-                raise(TypeError(msg))
-                
-    def add_bbox(self, bboxes, info=None):
-        info = info or [None for bbox in bboxes]
-        for i, bbox in enumerate(bboxes):
-            feature = Feature(data=self.crop(bbox))
-            feature.x_p = bbox[0]
-            feature.y_p = bbox[1]
-            feature.deserialize(info[i])
-            self._features.append(feature)
-            self._bboxes.append(bbox)         
-
-    def crop(self, bbox):
-        if self.image is None:
-            return None
-        (x, y, w, h) = bbox
-        center = ( int(np.round(x)), int(np.round(y)) )
-        cropshape = (w, h)
-        cropped, corner = crop_center(self.image, center, cropshape)
-        return cropped
+            elif feature is not None:
+                print('Warning: could not add feature {} of type {}'.format(i, type(feature)))
+#                 msg = "features must be list of Features"
+#                 msg += " or deserializable Features"
+#                 raise(TypeError(msg))
+        self.crop()                                                 #### find bboxes from features
     
+    def crop(self, all=False):
+        for i, bbox in enumerate(self.bboxes):
+            if bbox is not None:
+                feature = self._features[i]
+                if all or feature.data is None:
+                    if self.image is None:
+                        feature.data = None
+                    else:
+                    (x, y, w, h) = bbox
+                        center = ( int(np.round(x)), int(np.round(y)) )
+                        cropshape = (w, h)
+                        cropped, corner = crop_center(self.image, center, cropshape)
+                        feature.data = cropped
+                        feature.x_p = x if feature.x_p is None else feature.x_p
+                        feature.y_p = y if feature.y_p is None else feature.y_p
+                                            
     def optimize(self, report=True, **kwargs):
         for idx, feature in enumerate(self.features):
             result = feature.optimize(**kwargs)
