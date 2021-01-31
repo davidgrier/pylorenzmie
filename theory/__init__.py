427a1a5dddd5a986a390518e50664d03160e0148
David Grier <david.grier@nyu.edu>
2021-01-31 10:52:30 -0500

    cupy import controlled by configuration.py

--- theory/__init__.py
+++ theory/__init__.py
@@ -1,11 +1,23 @@
+import pylorenzmie.utilities.configuration as config
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
 from .Particle import Particle
 from .Sphere import Sphere
 
 from .Instrument import (Instrument, coordinates)
 
 try:
+    if not config.use_cupy:
+        raise ImportError('Cupy deselected in {}'.format(config.__file__))
     from .cupyLorenzMie import cupyLorenzMie as LorenzMie
-except:
+except ImportError as ex:
+    logger.warn('Cannot import cupyLorenzMie:' +
+                '\n\t{}'.format(ex) +
+                '\n\tFalling back to LorenzMie')
     from .LorenzMie import LorenzMie
 
 from .LMHologram import LMHologram
