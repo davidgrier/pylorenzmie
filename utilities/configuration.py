63ffabbfd9c86a2353cfdba0b32de689b3dacba1
David Grier <david.grier@nyu.edu>
2021-02-08 14:03:20 -0500

    consistent operation with 3.7 and 3.8

--- utilities/configuration.py
+++ utilities/configuration.py
@@ -1,9 +1,9 @@
 # Configuration for optional components of pylorenzmie
-use_numba = True
-use_cupy = True
+use_numba = False
+use_cupy = False
 
 
-#
+# Implement requested configuration
 import logging
 logging.basicConfig()
 logger = logging.getLogger(__file__)
@@ -12,38 +12,36 @@ logger.setLevel(logging.WARNING)
 try:
     import numba
     import_numba = True
-except ImportError as numba_ex:
+except ImportError as ex:
+    numba_ex = ex
     import_numba = False
-    logger.warn('Cannot import numba:\n\t{}'.format(numba_ex))
 
 
 def has_numba():
     if not use_numba:
-        logger.warn('Numba deselected in {}'.format(__file__))
-    #if not import_numba:
-        #logger.warn('Cannot import numba:\n\t{}'.format(numba_ex))
+        logger.warn(' numba deselected in {}'.format(__file__))
+    elif not import_numba:
+        logger.warn(' Cannot import numba:\n\t{}'.format(numba_ex))
     ok = use_numba and import_numba
     if not ok:
-        logger.warn('Falling back to standard implementation')
+        logger.warn(' Falling back to standard implementation')
     return ok
 
 
 try:
     import cupy
     import_cupy = True
-except ImportError as cupy_ex:
+except ImportError as ex:
+    cupy_ex = ex
     import_cupy = False
-    logger.warn('Cannot import cupy:\n\t{}'.format(cupy_ex))
 
 
 def has_cupy():
     if not use_cupy:
-        logger.warn('cupy deselected in {}'.format(__file__))
-    #if not import_cupy:
-        #logger.warn('Cannot import cupy:\n\t{}'.format(cupy_ex))
+        logger.warn(' cupy deselected in {}'.format(__file__))
+    elif not import_cupy:
+        logger.warn(' Cannot import cupy:\n\t{}'.format(cupy_ex))
     ok = use_cupy and import_cupy
     if not ok:
-        logger.warn('Falling back to standard implementation')
+        logger.warn(' Falling back to standard implementation')
     return ok
-
-    
