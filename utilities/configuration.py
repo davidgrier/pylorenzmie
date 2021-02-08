50e6900f37daeab259d894ed0176c221bb84f20e
laltman2 <laurenealtman@gmail.com>
2021-02-08 13:32:07 -0500

    bug fix

--- utilities/configuration.py
+++ utilities/configuration.py
@@ -14,13 +14,14 @@ try:
     import_numba = True
 except ImportError as numba_ex:
     import_numba = False
+    logger.warn('Cannot import numba:\n\t{}'.format(numba_ex))
 
 
 def has_numba():
     if not use_numba:
         logger.warn('Numba deselected in {}'.format(__file__))
-    if not import_numba:
-        logger.warn('Cannot import numba:\n\t{}'.format(numba_ex))
+    #if not import_numba:
+        #logger.warn('Cannot import numba:\n\t{}'.format(numba_ex))
     ok = use_numba and import_numba
     if not ok:
         logger.warn('Falling back to standard implementation')
@@ -32,13 +33,14 @@ try:
     import_cupy = True
 except ImportError as cupy_ex:
     import_cupy = False
+    logger.warn('Cannot import cupy:\n\t{}'.format(cupy_ex))
 
 
 def has_cupy():
     if not use_cupy:
         logger.warn('cupy deselected in {}'.format(__file__))
-    if not import_cupy:
-        logger.warn('Cannot import cupy:\n\t{}'.format(cupy_ex))
+    #if not import_cupy:
+        #logger.warn('Cannot import cupy:\n\t{}'.format(cupy_ex))
     ok = use_cupy and import_cupy
     if not ok:
         logger.warn('Falling back to standard implementation')
