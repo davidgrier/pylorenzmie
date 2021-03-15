1b096e802d53d381cada81bc9d84fdf51b760e3d
David Grier <david.grier@nyu.edu>
2021-03-15 11:36:28 -0400

    initial commit

--- detection/__init__.py
+++ detection/__init__.py
@@ -2,9 +2,10 @@ import pylorenzmie.utilities.configuration as config
 
 if config.has_catch():
     from .catchLocalizer import catchLocalizer as Localizer
+    from .catchEstimator import catchEstimator as Estimator
 else:
     from .Localizer import Localizer
-from .Estimator import Estimator
+    from .Estimator import Estimator
 
 
 __all__ = [Localizer, Estimator]
