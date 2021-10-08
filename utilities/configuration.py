e3de9695bea0bba7d731fbcfa0cf65b240fa643c
davidgrier <david.grier@nyu.edu>
2021-10-08 08:22:40 -0400

    simplify configuration

--- utilities/configuration.py
+++ utilities/configuration.py
@@ -1,7 +1,7 @@
 from importlib import import_module
 import logging
 
-
+use_cupy = True
 use_numba = True
 use_catch = True
 
@@ -12,21 +12,19 @@ logger.setLevel(logging.INFO)
 
 def has_(module):
     '''Return True if module is selected and can be imported'''
-    flag = globals()['use_'+module.lower()] or False
+    selected = globals()['use_'+module.lower()]
     can_import = False
-    if flag:
+    if selected:
         try:
             import_module(module)
             can_import = True
         except ImportError as ex:
-            logger.warn(' Cannot import {}:\n\t{}'.format(module, ex))
+            logger.warn(f' Cannot import {module}:\n\t{ex}')
     else:
-        logger.info(' {} deselected in {}'.format(module, __file__))
-    ok = flag and can_import
-    if not ok:
-        logger.info(' Falling back to standard implementation')
-    return ok
+        logger.info(f' {module} deselected in {__file__}')
+    return selected and can_import
 
 
+has_cupy = lambda: has_('cupy')
 has_numba = lambda: has_('numba')
 has_catch = lambda: has_('CATCH')
