61098a3eb48aebe34bcc8e1e81e7901c44cec0d0
Kaitlynn Theurkauf <kaitlynntheurkauf@gmail.com>
2022-09-02 20:34:27 -0400

    Added typing hints

--- theory/Instrument.py
+++ theory/Instrument.py
@@ -49,22 +49,6 @@ class Instrument(object):
     noise: float = 0.05
     darkcount: float = 0.    
 
-    """
-    def __init__(self,
-                 wavelength=0.532,
-                 magnification=0.135,
-                 n_m=1.335,
-                 background=1.,
-                 noise=0.05,
-                 dark_count=0.,
-                 **kwargs):
-        self.wavelength = wavelength
-        self.magnification = magnification
-        self.n_m = n_m
-        self.noise = noise
-        self.dark_count = dark_count
-        self.background = background
-    """ 
     def __str__(self):
         fmt = '<{}(wavelength={}, magnification={}, n_m={})>'
         return fmt.format(self.__class__.__name__,
@@ -75,72 +59,20 @@ class Instrument(object):
     def __repr__(self):
         return self.__str__()
 
-    """
     @property
-    def wavelength(self):
-        '''Wavelength of light in vacuum [um]'''
-        return self._wavelength
-
-    @wavelength.setter
-    def wavelength(self, wavelength):
-        logger.debug('wavelength: {} [um]'.format(wavelength))
-        self._wavelength = float(wavelength)
-
-    @property
-    def magnification(self):
-        '''Magnification of microscope [um/pixel]'''
-        return self._magnification
-
-    @magnification.setter
-    def magnification(self, magnification):
-        logger.debug('magnification: {} [um/pixel]'.format(magnification))
-        self._magnification = float(magnification)
-
-    @property
-    def n_m(self):
-        '''Complex refractive index of medium'''
-        return self._n_m
-
-    @n_m.setter
-    def n_m(self, n_m):
-        logger.debug('medium index: {}'.format(n_m))
-        self._n_m = float(n_m)
-
-    @property
-    def background(self):
-        '''Background image'''
-        return self._background
-
-    @background.setter
-    def background(self, background):
-        self._background = background
-
-    @property
-    def dark_count(self):
-        '''Dark count of camera'''
-        return self._dark_count
-
-    @dark_count.setter
-    def dark_count(self, dark_count):
-        assert dark_count >= 0, 'dark count is non-negative'
-        self._dark_count = dark_count
-
-    """
-    
-    @property
-    def properties(self):
+    def properties(self) -> dict:
         props = dict(n_m=self.n_m,
                      wavelength=self.wavelength,
                      magnification=self.magnification)
         return props
 
     @properties.setter
-    def properties(self, properties):
+    def properties(self, properties: dict) -> None:
         for property, value in properties.items():
             if hasattr(self, property):
                 setattr(self, property, value)
 
-    def dumps(self, **kwargs):
+    def dumps(self, **kwargs) -> str:
         '''Returns JSON string of adjustable properties
 
         Parameters
@@ -154,17 +86,17 @@ class Instrument(object):
         '''
         return json.dumps(self.properties, **kwargs)
 
-    def loads(self, str):
-        '''Loads JSON strong of adjustable properties
+    def loads(self, jproperties: str) -> None:
+        '''Loads JSON string of adjustable properties
 
         Parameters
         ----------
-        str : string
+        jproperties : string
             JSON-encoded string of properties
         '''
-        self.properties = json.loads(str)
+        self.properties = json.loads(jproperties)
 
-    def wavenumber(self, in_medium=True, magnified=True):
+    def wavenumber(self, in_medium: bool = True, magnified: bool = True) -> float:
         '''Return the wave number of light
 
         Parameters
