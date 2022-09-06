468acc7cd7df322be14bd341f69dbdf5b98c3ce8
davidgrier <david.grier@nyu.edu>
2022-09-06 14:24:54 -0400

    working version w/o aberrations

--- theory/LMHologram.py
+++ theory/LMHologram.py
@@ -54,15 +54,43 @@ class LMHologram(LorenzMie):
         hologram : numpy.ndarray
             Computed hologram.
         '''
-        try:
-            field = self.alpha * self.lorenzmie.field()
-        except TypeError:
-            return None
-        field[0, :] += self.aberrations.field()
+        field = self.alpha * self.field()
+        field[0, :] += 1.
         hologram = np.sum(np.real(field * np.conj(field)), axis=0)
         return hologram
 
 
 def main():
-    a = LMHologram()
+    from pylorenzmie.utilities import coordinates
+    from pylorenzmie.theory import (Sphere, Instrument)
+    import matplotlib.pyplot as plt
+
+    # Create coordinate grid for image
+    shape = (201, 201)
+    coords = coordinates(shape)
+    # Place two spheres in the field of view, above the focal plane
+    pa = Sphere()
+    pa.r_p = [150, 150, 200]
+    pa.a_p = 0.5
+    pa.n_p = 1.45
+    pb = Sphere()
+    pb.r_p = [100, 10, 250]
+    pb.a_p = 1.
+    pb.n_p = 1.45
+    particle = [pa, pb]
+    # Form image with default instrument
+    instrument = Instrument()
+    instrument.magnification = 0.048
+    instrument.wavelength = 0.447
+    instrument.n_m = 1.340
+
+    a = LMHologram(coordinates=coords,
+                   particle=particle,
+                   instrument=instrument)
     print(a)
+    plt.imshow(a.hologram().reshape(shape), cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- theory/LorenzMie.py
+++ theory/LorenzMie.py
@@ -180,16 +180,20 @@ class LorenzMie(LMObject):
             if hasattr(self, property):
                 setattr(self, property, value)
 
-    def field(self, cartesian: bool = True, bohren: bool = True) -> np.ndarray:
+    def field(self,
+              cartesian: bool = True,
+              bohren: bool = True) -> np.ndarray:
         '''Return field scattered by particles in the system'''
         if (self.coordinates is None or self.particle is None):
             return None
-        self.result.fill(0.+0.j)
         k = self.instrument.wavenumber()
+        n_m = self.instrument.n_m
+        wavelength = self.instrument.wavelength
+        self.result.fill(0.+0.j)
         for p in np.atleast_1d(self.particle):
             dr = self.coordinates - p.r_p[:, None]
             self.krv[...] = np.asarray(k * dr)
-            ab = p.ab(self.instrument.n_m, self.instrument.wavelength)
+            ab = p.ab(n_m, wavelength)
             this = self.compute(ab, self.krv, *self.buffers,
                                 cartesian=cartesian, bohren=bohren)
             this *= np.exp(-1j * k * p.z_p)
