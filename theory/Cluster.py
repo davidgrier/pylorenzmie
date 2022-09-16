d9fff1b9759c6b75d414f880d5a3e46a138427cc
davidgrier <david.grier@nyu.edu>
2022-09-16 19:27:07 -0400

    initial broken implementation

--- theory/Cluster.py
+++ theory/Cluster.py
@@ -8,9 +8,13 @@ class Cluster(Particle):
 
     particles: list = field(repr=False, default_factory=list)
 
+    def __post_init__(self):
+        self.update()
+
     def __setattr__(self, key: str, value: Any) -> None:
         super().__setattr__(key, value)
         if key in ['x_p', 'y_p', 'z_p', 'particles']:
+            print(key, value)
             self._update()
 
     def __iter__(self) -> iter:
@@ -28,3 +32,6 @@ class Cluster(Particle):
                 particle.r_0 = self.r_p
         except AttributeError:
             pass
+
+    def update(self) -> None:
+        self._update()
