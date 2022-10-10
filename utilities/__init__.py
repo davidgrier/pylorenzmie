02699b60d5050e43eb54899a5572bd2453261480
davidgrier <david.grier@nyu.edu>
2022-10-10 11:08:04 -0400

    class implementation

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,6 +1,8 @@
 from .azimuthal import (aziavg, azistd, azimedian, azimad)
+from .circletransform import (circletransform, Circletransform)
 from .coordinates import coordinates
 from .h5video import h5video
 
 
-__all__ = 'aziavg azistd azimedian azimad coordinates, h5video'.split()
+__all__ = 'aziavg azistd azimedian azimad' \
+          'circletransform Circletransform coordinates h5video'.split()
