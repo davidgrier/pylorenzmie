6e2d3a0b058ef4abc995abe78ce60d527aed6160
davidgrier <david.grier@nyu.edu>
2022-10-10 10:13:48 -0400

    new azi functions

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,8 +1,6 @@
-from .aziavg import aziavg
-from .azistd import azistd
-from .azimedian import azimedian
+from .azimuthal import (aziavg, azistd, azimedian, azimad)
 from .coordinates import coordinates
 from .h5video import h5video
 
 
-__all__ = [aziavg, azistd, azimedian, coordinates, h5video]
+__all__ = 'aziavg azistd azimedian azimad coordinates, h5video'.split()
