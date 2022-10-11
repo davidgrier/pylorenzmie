9753cc2a9bbff96f53f727b3e04c1e1b86efe3d6
davidgrier <david.grier@nyu.edu>
2022-10-11 10:53:50 -0400

    return dataframes

--- analysis/Estimator.py
+++ analysis/Estimator.py
@@ -3,6 +3,7 @@ from pylorenzmie.utilities import aziavg
 from scipy.signal import (savgol_filter, argrelmin)
 from scipy.stats import sigmaclip
 from scipy.special import jn_zeros
+import pandas as pd
 
 
 class Estimator(object):
@@ -19,8 +20,8 @@ class Estimator(object):
 
     Methods
     -------
-    predict(feature) :
-        Returns a dictionary of estimated properties
+    predict(feature) : pandasSeries
+        Returns estimated properties
     '''
     def __init__(self,
                  a_p=1.,
@@ -36,19 +37,17 @@ class Estimator(object):
     def _initialize(self, feature):
         '''Prepare for estimation
 
-        self.k: wavenumber in the medium [pixels^{-1}]
+        self.k: wavenumber in the medium [radian/pixels]
         self.noise: noise estimate from instrument
         self.profile: aximuthal average of data
         '''
         self._initialized = False
         self.feature = feature
-        if feature is None:
+        if (feature is None) | (feature.data is None):
             return
-        if feature.data is None:
-            return
-        ins = self.feature.model.instrument
-        self.k = (2.*np.pi * ins.n_m / ins.wavelength) * ins.magnification
-        self.noise = ins.noise
+        instrument = feature.model.instrument
+        self.k = instrument.wavenumber()
+        self.noise = instrument.noise
         center = np.array(self.feature.data.shape) // 2
         self.profile = aziavg(self.feature.data, center) - 1.
         self._initialized = True
@@ -64,7 +63,7 @@ class Estimator(object):
         a = self.profile
         rho = np.arange(len(a)) + 0.5
         lap = savgol_filter(a, 11, 3, 2) + savgol_filter(a, 11, 3, 1)/rho
-    
+
         good = np.abs(a) > self.noise
         qsq = -lap[good] / a[good] / self.k**2
         rho = rho[good]
@@ -74,29 +73,29 @@ class Estimator(object):
         qsq = qsq[good]
 
         zsq = rho**2 * (1./qsq - 1.)
-    
+
         return np.sqrt(np.mean(sigmaclip(zsq).clipped))
 
     def _estimate_a(self, z_p):
         '''Estimate radius of particle
 
-        Model interference pattern as spherical wave 
+        Model interference pattern as spherical wave
         eminating from z_p and interfering with a plane wave.
         '''
         if not self._initialized:
             return self.a_p
-        instrument = self.feature.model.instrument
+        magnification = self.feature.model.instrument.magnification
         minima = argrelmin(self.profile)
         alpha_n = np.sqrt((z_p/minima)**2 + 1.)
         a_p = np.median(jn_zeros(1, len(alpha_n)) * alpha_n) / self.k
-        return 2. * instrument.magnification * a_p
-    
+        return 2. * magnification * a_p
+
     def predict(self, feature=None):
         if feature is not None:
             self._initialize(feature)
         z_p = self._estimate_z()
         center = np.mean(self.feature.coordinates, axis=1)
-        r_p = [center[0], center[1], z_p]
         a_p = self._estimate_a(z_p)
         n_p = self.n_p
-        return dict(r_p=r_p, a_p=a_p, n_p=n_p)
+        return pd.Series({'x_p': center[0], 'y_p': center[1],
+                          'z_p': z_p, 'a_p': a_p, 'n_p': n_p})
