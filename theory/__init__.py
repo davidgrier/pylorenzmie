bf56c9eea5f0b397207726b8f1ae08088e987448
David Grier <david.grier@nyu.edu>
2023-02-10 12:56:53 -0500

    simplified factory model for aberrated scattering theories

--- theory/__init__.py
+++ theory/__init__.py
@@ -3,11 +3,12 @@ from .Sphere import Sphere
 from .Cluster import Cluster
 from .Dimer import Dimer
 from .Instrument import Instrument
-from .AberratedLorenzMie import AberratedLorenzMie
+from .Aberrated import (Aberrated, AberratedLorenzMie)
 from .LorenzMie import LorenzMie
 
 # Alias for backward compatibility
 LMHologram = LorenzMie
 
 __all__ = 'Instrument Particle Sphere Cluster Dimer' \
-          'LorenzMie AberratedLorenzMie LMHologram'.split()
+          'LorenzMie LMHologram' \
+          'Aberrated AberratedLorenzMie'.split()
