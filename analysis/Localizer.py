e93598a5d16db5824f9930d326e4d4e8d2dc68f0
David Grier <david.grier@nyu.edu>
2025-08-01 18:28:41 -0400

    improved zero crossing detector

--- analysis/Localizer.py
+++ analysis/Localizer.py
@@ -1,11 +1,9 @@
 from pylorenzmie.lib import (LMObject, Azimuthal, CircleTransform)
 import numpy as np
-from numpy.typing import NDArray
 import trackpy as tp
 import pandas as pd
 
 
-Image = NDArray[int | float]
 Prediction = pd.DataFrame
 
 
@@ -38,21 +36,21 @@ class Localizer(LMObject):
     '''
 
     def __init__(self,
-                 diameter: int | None = None,
-                 nfringes: int | None = None,
+                 diameter: int = 31,
+                 nfringes: int = 20,
                  **kwargs) -> None:
-        self.diameter = diameter or 31
-        self.nfringes = nfringes or 20
+        self.diameter = diameter
+        self.nfringes = nfringes
         self._circletransform = CircleTransform()
         self.detect = self.localize
 
     @LMObject.properties.fget
-    def properties(self) -> dict:
+    def properties(self) -> LMObject.Properties:
         keys = 'nfringes diameter'.split()
         return {k: getattr(self, k) for k in keys}
 
     def localize(self,
-                 image: Image | list[Image],
+                 image: LMObject.Image | list[LMObject.Image],
                  diameter: int | None = None,
                  nfringes: int | None = None,
                  **kwargs) -> Prediction:
@@ -90,9 +88,11 @@ class Localizer(LMObject):
         for n, feature in features.iterrows():
             r_p = feature[['x', 'y']].to_numpy()
             b = Azimuthal.avg(image, r_p)
-            p = b > 1.
-            ndx = np.where(p[1:] ^ p[:-1])[0] + 1
-            extent = ndx[nfringes] if len(ndx) > nfringes else ndx[-1]
+            ndx = np.where(np.diff(np.sign(b - 1.)) != 0)[0] + 1
+            try:
+                extent = ndx[nfringes]
+            except IndexError:
+                extent = ndx[-1]
             r0 = tuple((r_p - extent/2).astype(int))
             bbox = (r0, extent, extent)
             prediction = dict(x_p=r_p[0], y_p=r_p[1], bbox=bbox)
@@ -100,13 +100,13 @@ class Localizer(LMObject):
         return pd.DataFrame(predictions)
 
     @classmethod
-    def example(cls) -> None:
+    def example(cls: 'Localizer') -> None:
         import cv2
         import matplotlib
         from matplotlib import pyplot as plt
         from matplotlib.patches import Rectangle
 
-        # Create a Localizer
+        # Instantiate a Localizer
         localizer = cls()
 
         # Normalized hologram
@@ -121,7 +121,6 @@ class Localizer(LMObject):
 
         # Show and report results
         style = dict(fill=False, linewidth=3, edgecolor='r')
-        matplotlib.use('TkAgg')
         fig, ax = plt.subplots()
         ax.imshow(b, cmap='gray')
         for bbox in features.bbox:
