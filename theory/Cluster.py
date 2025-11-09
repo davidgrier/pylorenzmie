587d288f97e39bfd4f4ce01f63afa50d8e26bbf5
David Grier <david.grier@nyu.edu>
2025-11-09 12:55:48 -0500

    clusters are iterable

--- theory/Cluster.py
+++ theory/Cluster.py
@@ -5,9 +5,10 @@ from dataclasses import dataclass, field
 @dataclass
 class Cluster(Particle):
 
-    particles: list = field(repr=False, default_factory=list)
+    particles: list[Particle] = field(repr=False, default_factory=list)
 
     def __post_init__(self) -> None:
+        super().__post_init__()
         self.update()
 
     def __setattr__(self, key: str, value: Particle.Property) -> None:
@@ -16,13 +17,22 @@ class Cluster(Particle):
             self._update()
             return
 
-    def __iter__(self) -> iter:
-        return iter(self.particles)
-
     def __len__(self) -> int:
+        print(len(self.particles))
         return len(self.particles)
 
+    def __next__(self) -> Particle:
+        if self._index < len(self.particles):
+            result = self.particles[self._index]
+            self._index += 1
+            return result
+        else:
+            self._index = 0
+            raise StopIteration
+
     def __getitem__(self, index: int) -> Particle:
+        if index < 0 or index >= len(self):
+            raise IndexError('Particle index out of range')
         return self.particles[index]
 
     def _update(self) -> None:
