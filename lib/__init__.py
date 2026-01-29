70b01f8d90c6cc4445669ee0c2a9a1328aeeab78
David Grier <david.grier@nyu.edu>
2026-01-29 12:54:50 -0500

    provided coordinates for backward compatibility

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,14 @@
 from .LMObject import LMObject
 from .CircleTransform import (CircleTransform, circletransform)
+from warnings import warn
+
+
+def coordinates(*args, **kwargs):
+    warn('''
+    coordinates() is deprecated and will be removed in a future release.
+    Use LMObject.meshgrid() instead.
+    ''', DeprecationWarning, 2)
+    return LMObject.meshgrid(*args, **kwargs)
 
 
 __all__ = 'LMObject Azimuthal CircleTransform circletransform'.split()
